06159aa2722f947cf83b8a181ce4811504ec9b11
dvsrshohan <shohan.ridigames@gmail.com>
2022-10-27 17:30:24 +0600

    Style 404 Page

--- src/commponents/NotFund404/NotFund404.js
+++ src/commponents/NotFund404/NotFund404.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FaArrowCircleLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const NotFund404 = () => {
     return (
-        <div>
-            <img src="https://i.pinimg.com/originals/86/41/80/86418032b715698a4dfa6684b50c12af.gif" alt="" srcset="" />
+        <div className='text-center'>
+            <img className='w-50' src="https://i.pinimg.com/originals/86/41/80/86418032b715698a4dfa6684b50c12af.gif" alt="" srcset="" />
             <p>404 Page Not Found</p>
-            <button className='btn btn-success'><FaArrowCircleLeft> </FaArrowCircleLeft>Get All Courses</button>
+            <Link to='/courses'>
+                <button className='btn btn-success'><FaArrowCircleLeft /> Get All Courses</button>
+            </Link>
         </div>
     );
 };
